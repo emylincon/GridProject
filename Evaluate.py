efd042c35304e023a0d7fc712ba30db89dc8827c
Emeka Ugwuanyi <emylincon@gmail.com>
2020-12-22 22:27:00 +0000

    refactoring and added comments

--- Evaluate.py
+++ Evaluate.py
@@ -16,6 +16,11 @@ from tensorflow.keras.layers import Conv2D, Flatten, Dense
 
 
 class Predict:
+    """
+    This class uses the trained models to classify if an image can be classified as ec or nec
+    ec means eczema while nec means not eczema
+    Only the get_result method is public, it expects an image path and returns the result
+    """
     def __init__(self):
         self.height = 300
         self.width = 300
@@ -23,28 +28,28 @@ class Predict:
         self.eff_model = keras.models.load_model('models/eff11_model.h5')
         self.inception_model = keras.models.load_model('models/inception_model.h5')
 
-    def resnet(self, x):
+    def __resnet(self, x):
         x = resnet50_preprocess(x)
         preds = self.resnet_model.predict(x)
         return self.format_res(preds)
 
-    def efficient(self, x):
+    def __efficient(self, x):
         x = eff_preprocess(x)
         preds = self.eff_model.predict(x)
         return self.format_res(preds)
 
-    def inception(self, x):
+    def __inception(self, x):
         x = incept_preprocess(x)
         preds = self.inception_model.predict(x)
         return self.format_res(preds)
 
     @staticmethod
-    def format_res(pred):
+    def format_res(pred):  # formats output result to percentage
         pred = list((pred*100)[0])
         pred = [float(round(i, 2)) for i in pred]
         return pred
 
-    def get_x(self, image_path, case=None):
+    def __get_x(self, image_path, case=None):
         if case == 'inception':
             img = load_img(image_path, target_size=(224, 224))
             x = img_to_array(img)
@@ -57,11 +62,11 @@ class Predict:
             return x
 
     def get_result(self, image_path):
-        x = self.get_x(image_path=image_path)
-        xx = self.get_x(case='inception', image_path=image_path)
-        return {'resnet': self.resnet(x),
-                'efficient': self.efficient(x),
-                'inception': self.inception(xx)}
+        x = self.__get_x(image_path=image_path)
+        xx = self.__get_x(case='inception', image_path=image_path)
+        return {'resnet': self.__resnet(x),
+                'efficient': self.__efficient(x),
+                'inception': self.__inception(xx)}
 
 
 
