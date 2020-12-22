bb9b7bafd8336ddcde90184d2ad5c113febc4730
Emeka Ugwuanyi <emylincon@gmail.com>
2020-12-22 23:06:43 +0000

    refactoring

--- web.py
+++ web.py
@@ -1,8 +1,6 @@
-from flask import Flask, render_template, session, url_for, request, jsonify, Response
-from pywget import wget
+from flask import Flask, render_template, url_for, request, jsonify, Response
 import base64
 import codecs as c
-import os
 import time
 from werkzeug.utils import secure_filename
 from Evaluate import Predict
@@ -31,24 +29,11 @@ def upload():
     return render_template('upload.html')
 
 
-def fin2(base_string, file_name):  #this returns a image
+def fin2(base_string, file_name):  # this writes the image to disk
     str_obj = base_string.split('base64')[1][1:].encode()
     fh = open(file_name, "wb")
     fh.write(c.decode(obj=str_obj, encoding='base64'))
     fh.close()
-
-
-def download(url):
-    filename = wget.download(url, 'static/temp')
-    # os.rename(filename, filename+'.jpg')
-    return filename
-
-
-@app.route('/tik/<path:tok>')
-def tik(tok):
-    print('hey')
-    print(tok)
-    return jsonify({1:2})
 
 
 @app.route('/process/<path:img>')
